125c3ccf75e28756b9d0834602711b12c7959632
doctryucsd <chy036@ucsd.edu>
2025-08-19 02:17:37 +0800

    fix locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change REPO_NAME to your repo (e.g., 'portfolio')
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Personal-Website/' : '/',  // user site: just '/'
+export default defineConfig({
+  base: '/',            // keep root base for local dev & local preview
   plugins: [react()],
-}))
+})
